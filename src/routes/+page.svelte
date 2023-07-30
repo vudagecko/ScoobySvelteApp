0862b8ec48e1f3a350699295975a30cefbb7683a
Matt |2ickman <github@vudagecko.com>
2023-07-30 14:02:30 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { Alert, Card } from 'flowbite-svelte';
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+<div class="p-8">
+	<Alert>
+		<span class="font-medium">Scooby Apps Template!!</span> This is an app template that uses SvelteKit, Tailwind CSS, Flowbite.
+	</Alert>
+</div>
+
+
+<Card class="max-w-sm" imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
+    <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      Noteworthy technology acquisitions 2021
+    </h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+  </Card>
